cfb9135e2990d0556cd750b990fc2fe96dd67969
kunyoungPark <p3145@daum.net>
2023-04-03 12:07:51 +0900

    bfs_perm

--- programmers/DFS/target_number.py
+++ programmers/DFS/target_number.py
@@ -1,33 +1,26 @@
-
-
-def bfs_perm(L):
+def bfs_perm():
     global cnt
-    if L == n1:
-        if operate(n, opArr) == target:
-            cnt += 1
-        return
+    leaves = [0]
+    for n in numbers:
+        temp = []
+        for p in leaves:
+            temp.append(p + n)
+            temp.append(p - n)
+        leaves = temp
+    cnt = leaves.count(target)
+    return cnt
 
-    for 
 
-def operate(n, o):
-    ans = n[0]
-    for i in range(1,n):
-        #0 => + 1=> -
-        if o[i-1] == 0:
-            ans += n[i]
-        else:
-            ans -= n[i]
-    return ans
-
-#sample data 1
+# sample data 1
 numbers = [1, 1, 1, 1, 1]
 target = 3
 ans1 = 5
-#sample data 2
+# sample data 2
 numbers2 = [4, 1, 2, 1]
 target2 = 4
 ans2 = 2
 
-n1 = len(numbers) - 1
-os = [0] * len(numbers)
+nn = len(numbers)
 cnt = 0
+v = [0] * nn
+print(bfs_perm())
